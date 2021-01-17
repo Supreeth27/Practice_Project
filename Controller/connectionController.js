920b2f0a920ecdb046428bd905d91edbe50619df
Supreeth <supreethsai.s.27@gmail.com>
2021-01-17 10:55:33 -0500

    Adding assets and home page

--- Controller/connectionController.js
+++ Controller/connectionController.js
@@ -2,4 +2,19 @@ const createError = require('http-errors');
 const express = require('express');
 var session = require('express-session');
 const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+router.get('/about', function (req, res) {
+    res.render('about');
+});
+router.get('/contact', function (req, res) {
+    res.render('contact');
+});
+router.get('/*', function(req, res) {
+    res.render('index');
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connectionController = require('./Controller/connectionController');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
 
 app.use(session({
@@ -24,14 +24,6 @@ app.use(express.static(path.join(__dirname, 'assets')));
 
 // routes
 app.use('/', connectionController);
-app.use('/connections', connectionController);
-app.use('/connection/:connectionId', connectionController);
-app.use('/myConnections', connectionController);
-app.use('/newConnection', connectionController);
-
-app.use('/about', connectionController);
-app.use('/contact', connectionController);
-
 
 // end of error handling
 app.listen(8080);
